5e62edc1636ab2cb70bf688b98dd41d907d18e62
Sangram <sangram2mail@gmail.com>
2026-06-21 10:15:22 +0530

    Delayed Jobs

--- producer.js
+++ producer.js
@@ -2,7 +2,7 @@ const { Queue } = require('bullmq');
 const IORedis = require('ioredis');
 
 //1. Establish connection to Redis
-const connection = new IORedis({host: '127.0.0.1',port: 6379});
+const connection = new IORedis({host: '127.0.0.1',port: 6379, maxRetriesPerRequest: null});
 
 //2. Initialize the queue 
 const myQueue = new Queue('email-queue', {connection});
@@ -21,3 +21,28 @@ async function addJob(){
 }
 
 addJob();
+
+async function addDelayJob(){
+
+    //Define Delay in millisecond
+
+    const TWO_HOURS = 2 * 60 * 60 * 1000;
+
+    const job = await myQueue.add(
+        'cart-abandonment-email',
+        {
+            userId: 'user_987',
+            cartId: 'cart_abc',
+            email: 'shopper@example.com',
+        },
+        {
+            delay: TWO_HOURS,
+        }
+    );
+    console.log(`Delayed job added! ID: ${job.id}`);
+    process.exit(0);
+}
+addDelayJob();
+
+
+// await job.remove() --> to delete a delayed job
