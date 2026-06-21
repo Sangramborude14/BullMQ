f048d3a6c93010617d79275bdb0d60bd9296666a
Sangram <sangram2mail@gmail.com>
2026-06-21 10:02:29 +0530

    Cocurrency

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ const { Worker } = require('bullmq');
 const IORedis = require('ioredis');
 
 //1. Establish connection to Redis
-const connection = new IORedis({host: '127.0.0.1',port: 6379});
+const connection = new IORedis({host: '127.0.0.1',port: 6379, maxRetriesPerRequest: null});
 
 //2. Initialize the worker
 const worker = new Worker('email-queue', async (job) => {
@@ -12,10 +12,14 @@ console.log(`[Worker]  Processing job ${job.id} of type: ${job.name}`);
 console.log(`[Worker] Sending email to ${job.data.email}...`);
 
 // Simulating an async function operation
-await new Promise   ((resolve) => setTimeout(resolve, 2000));
+await new Promise((resolve) => setTimeout(resolve, 2000));
 
 console.log(`[Worker] Job ${job.id} completed`)
-},{connection});
+},{connection,
+
+    concurrency: 5 // This worker will now process up to 5 jobs at a time
+
+});
 
 
 worker.on('completed',(job,err) => {
